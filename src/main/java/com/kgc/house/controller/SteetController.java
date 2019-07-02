11de17b8ab232e8e4fe859fce55d7bc1ca761dcb
Crush-a <774270398@qq.com>
2019-07-02 15:24:07 +0800

    下

--- src/main/java/com/kgc/house/controller/SteetController.java
+++ src/main/java/com/kgc/house/controller/SteetController.java
@@ -32,7 +32,7 @@ public class SteetController {
     public Map<String,Object> getUsers(Integer page,Integer rows,Integer did){
         //调用业务
         PageInfo<Street> pageInfo=streetService.getStreetByDistrict(page,rows,did);
-        Map<String,Object> map=new HashMap<>();
+        Map<String,Object> map=new HashMap<String,Object>();
         map.put("total",pageInfo.getTotal());
         map.put("rows",pageInfo.getList());
         return map;
