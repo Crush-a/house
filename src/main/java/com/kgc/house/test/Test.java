23fa60e7d9fe1785fd6df6a40d27d1dda10643e5
Crush <774270398@qq.com>
2019-11-12 16:21:09 +0800

    d

--- src/main/java/com/kgc/house/test/Test.java
+++ src/main/java/com/kgc/house/test/Test.java
@@ -1,16 +1,19 @@
 package com.kgc.house.test;
 
+import com.kgc.house.entity.District;
+import com.kgc.house.service.DistrictService;
+
 import java.util.UUID;
 
 public class Test {
     public static void main(String[] args) {
-       /* ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
         DistrictService service= (DistrictService)ctx.getBean("districtServiceImpl");
         PageInfo<District> pageInfo= service.getDistrictByPage(1,3);
 
         for (District d : pageInfo.getList()) {
             System.out.println(d.getName());
-        }*/
+        }
         System.out.println(UUID.randomUUID());
     }
 }
